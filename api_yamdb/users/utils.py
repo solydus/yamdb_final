e8327b5a23100c0c60116e4a7edb316cff6e79d8
solydus <97594065+solydus@users.noreply.github.com>
2023-05-30 13:17:53 +0300

    Update utils.py

--- api_yamdb/users/utils.py
+++ api_yamdb/users/utils.py
@@ -9,8 +9,7 @@ LENGTH = 20
 
 def generate_confirmation_code():
     letters_and_digits = string.ascii_letters + string.digits
-    confirmation_code = ''.join(sample(letters_and_digits, LENGTH))
-    return confirmation_code
+    return ''.join(sample(letters_and_digits, LENGTH))
 
 
 def send_confirmation_code(email, confirmation_code):
